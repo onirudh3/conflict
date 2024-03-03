4c70f8a4c8d24c9d65ec34d6031d233289de35e2
Anirudh Ravishankar <onirudh3@gmail.com>
2024-03-03 14:27:24 +0100

    updates mar 3

--- script.R
+++ script.R
@@ -10,169 +10,8 @@ library(did)
 library(gtsummary)
 library(xtable)
 
-# Oil field data
-discoveries <- read.csv("Data/giant_fields_2018.csv")
-
-# How many countries in discoveries
-count(discoveries, COUNTRY) # 80 countries
-
-# Conflict data
-conflicts <- read_excel("Data/GEDEvent_v23_1.xlsx") %>% 
-  dplyr::select(c("id", "conflict_new_id", "country", "year", "type_of_violence", 
-                  "best"))
-
-conflicts$conflict_new_id <- as.factor(conflicts$conflict_new_id)
-
-# Population data from world bank
-pop_data <- read.csv("Data/population_data.csv")
-
-# GDP Data
-gdp <- read.csv("Data/gdp_cleaned.csv")
-
-# Political Stability data
-stability_data <- read.csv("Data/stability_data.csv")
-
-# How many countries in conflicts
-n_distinct(conflicts$country) # 124 countries
-
-
-# Country names -----------------------------------------------------------
-
-# Match country names in the two datasets
-discoveries <-discoveries %>%
-  mutate(COUNTRY = case_when(COUNTRY == "Sierre Leone" ~ "Sierra Leone", 
-                             COUNTRY == "Equatorial Guinea" ~ "Guinea",
-                             COUNTRY == "Congo (Brazzaville)" ~ "Congo",
-                             COUNTRY == "Norway and United Kingdom" ~ "Norway",
-                             COUNTRY == "Divided Neutral Zone: Kuwait/Saudi Arabia" ~ "Kuwait", # for simplicity
-                             TRUE ~ COUNTRY))
-conflicts <- conflicts %>% 
-  mutate(country = case_when(country == "Russia (Soviet Union)" ~ "Russia",
-                             country == "Myanmar (Burma)" ~ "Myanmar",
-                             country == "Yemen (North Yemen)" ~ "Yemen",
-                             country == "United States of America" ~ "United States",
-                             country == "DR Congo (Zaire)" ~ "Congo",
-                             country == "Guinea-Bissau" ~ "Guinea",
-                             country == "United Arab Emirates" ~ "UAE",
-                             TRUE ~ country))
-
-# Write to csv
-write.csv(conflicts, "Data/conflicts_raw.csv", row.names = F)
-write.csv(discoveries, "Data/discoveries_raw.csv", row.names = F)
-
-
-# Getting unique conflicts ------------------------------------------------
-
-conflictsuniq <- conflicts %>%
-  mutate(country_year = paste(country, year))
-
-# Total fatalities at country year level
-conflictsuniq <- conflictsuniq %>% 
-  group_by(country_year) %>% 
-  mutate(total_fatalities = sum(best))
-
-# Number of starting conflict events at country year level
-conflictsuniq <- conflictsuniq %>% 
-  group_by(country_year) %>% 
-  mutate(number_of_conflicts_started = n())
-
-# Aggregate to country year level
-conflictsuniq <- conflictsuniq %>% 
-  select(country, year, country_year, number_of_conflicts_started, total_fatalities) %>%
-  distinct(country_year, .keep_all = T) %>% 
-  arrange(country, year)
-
-
-# Building the panel dataset ----------------------------------------------
-
-# Cleaned dataset
-result_data <- expand.grid(year = seq(1989, 2022, 1), 
-                           country = unique(c(conflictsuniq$country, discoveries$COUNTRY)))
-
-# Numeric ID for each country
-result_data <- result_data %>%
-  group_by(country) %>%
-  mutate(country_ID = cur_group_id(),
-         .before = country)
-
-# Conflict dummy
-result_data$conflict_dummy <- 0
-result_data <- result_data %>% 
-  mutate(country_year = paste(country, year), .after = country)
-result_data <- result_data %>% 
-  mutate(conflict_dummy = case_when(country_year %in% conflictsuniq$country_year ~ 1, T ~ 0))
-
-# Discovery dummy
-result_data$discovery_dummy <- 0
-discoveries <- discoveries %>% 
-  mutate(country_year = paste(COUNTRY, year))
-result_data <- result_data %>% 
-  mutate(discovery_dummy = case_when(country_year %in% discoveries$country_year ~ 1, T ~ 0))
-
-
-# Merge conflict data -----------------------------------------------------
-
-# Merge number_of_conflicts_started and total_fatalities to result_data
-result_data <- left_join(result_data, subset(conflictsuniq, 
-                                             select = c("country_year", 
-                                                        "number_of_conflicts_started",
-                                                        "total_fatalities")), 
-                         by = "country_year")
-
-result_data <- result_data %>% 
-  mutate(number_of_conflicts_started = case_when(is.na(number_of_conflicts_started) ~ 0, 
-                                                 T ~ number_of_conflicts_started),
-         total_fatalities = case_when(is.na(total_fatalities) ~ 0,
-                                      T ~ total_fatalities))
-
-result_data <- result_data %>% 
-  group_by(country_year) %>% 
-  mutate(number_of_conflicts_started = sum(number_of_conflicts_started),
-         total_fatalities = sum(total_fatalities))
-
-# Remove duplicate rows
-result_data <- result_data %>% 
-  distinct()
-
-
-# Number of discoveries in a given country-year ---------------------------
-
-# Number of discoveries in a given country
-result_data <- result_data %>% 
-  group_by(country) %>% 
-  mutate(total_discoveries = sum(discovery_dummy))
-
-# What are the countries where there are no discoveries?
-n_distinct(subset(result_data, total_discoveries == 0)$country) # 78 countries
-
-
-# Time period indicator ---------------------------------------------------
-
-# Discovery period
-result_data <- result_data %>%
-  group_by(country) %>% 
-  mutate(period = row_number() - which(discovery_dummy == 1)[1],
-         .after = discovery_dummy)
-
-# Year of first discovery
-result_data <- result_data %>% 
-  group_by(country) %>%
-  mutate(first_discovery = `year`[period == 0],
-         .after = discovery_dummy)
-result_data <- result_data %>% 
-  mutate(first_discovery = case_when(is.na(first_discovery) ~ 0, T ~ first_discovery))
-
-
-# Merge population and GDP data -------------------------------------------
-
-result_data <- left_join(result_data, gdp)
-result_data <- left_join(result_data, pop_data)
-result_data <- left_join(result_data, stability_data)
-
-# We do not have population and stability data for Kyrgyzstan, so we remove it
-# View(result_data[!complete.cases(result_data$stability),])
-result_data <- subset(result_data, country != "Kyrgyzstan")
-
+# Read cleaned data
+df <- read.csv("Data/final_dataset.csv")
 
 # Summary Statistics ------------------------------------------------------
 
@@ -193,6 +32,7 @@ summary(subset(result_data, gdp_quartile %in% c(3, 4))$number_of_conflicts_start
 summary(subset(result_data, gdp_quartile %in% c(1, 2))$total_discoveries)
 summary(subset(result_data, gdp_quartile %in% c(1, 2))$number_of_conflicts_started)
 
+
 # Variable transformations ------------------------------------------------
 
 # Logs
@@ -202,8 +42,8 @@ result_data <- result_data %>%
 
 # Scale by population
 result_data <- result_data %>% 
-  mutate(scaled_number_of_conflicts_started = number_of_conflicts_started / pop * 10000,
-         scaled_total_fatalities = total_fatalities / pop * 100)
+  mutate(scaled_number_of_conflicts_started = number_of_conflicts_started / pop,
+         scaled_total_fatalities = total_fatalities / pop)
 
 
 # Checking correlation between conflicts and fatalities -------------------
